9000914bc514e97b16f5245e66cd3de91af3957f
anon-is-anonymus <cylee301203@gmail.com>
2023-09-24 03:07:38 +0800

    Update server launch commands

--- run_server.bash
+++ run_server.bash
@@ -6,7 +6,7 @@ read -p 'CS:GO Port (Default: 27015): | Leave blank if not needed ' csPort
 read -p 'GOTV Port (Default: 27020): | Leave blank if not needed ' gotvPort
 read -p 'RCON Password: | Leave blank if not needed ' rconPass
 read -p 'Server Password: | Leave blank if not needed ' serverPass
-read -p 'Game Server Token: ' gameToken
+read -p 'Game Server Token (See https://steamcommunity.com/dev/managegameservers): ' gameToken
 
 homeFolder=~
 
@@ -32,5 +32,5 @@ cleanUpFilePath $csLocation
 csLocation=$cleanString
 
 # Start server
-tmux new-session -s "CSGO_Server" -d "cd $csLocation && ./srcds_run -game csgo -console -port $csPort +rcon_password $rconPass +sv_password $serverPass +sv_setsteamaccount $gameToken +tv_port $gotvPort +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2"
+tmux new-session -s "CSGO_Server" -d "cd $csLocation && ./srcds_run -game csgo -console -tickrate 128 -sv_mincmdrate 128 -sv_minupdaterate 128 -port $csPort +rcon_password $rconPass +sv_password $serverPass +sv_setsteamaccount $gameToken +tv_port $gotvPort +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2"
 echo TMUX Server started!
